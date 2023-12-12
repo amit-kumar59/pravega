92356ae5f20badf7e1ebef4219082acb005f8ac1
amit-kumar59 <amit.kumar59@dell.com>
2023-12-12 10:47:25 +0530

    Added more logs for debug
    Signed-off-by: amit-kumar59 <amit.kumar59@dell.com>

--- controller/src/main/java/io/pravega/controller/server/ControllerService.java
+++ controller/src/main/java/io/pravega/controller/server/ControllerService.java
@@ -116,7 +116,12 @@ public class ControllerService {
         if (cluster == null) {
             return Futures.failedFuture(new IllegalStateException("Controller cluster not initialized"));
         }
-
+        log.info("************ ControllerService@getControllerServerList amit  start******************* ");
+        cluster.getClusterMembers().stream().forEach((host) -> {
+            log.info("ip address ::Host", host.toString());
+            log.info("ip address ::{} port :{} host id::{} endPoint ID ::{}", host.getIpAddr(), host.getPort(), host.getHostId(), host.getEndpointId());
+        });
+        log.info("************ ControllerService@getControllerServerList amit  end******************* ");
         return CompletableFuture.supplyAsync(() -> {
             try {
                 return cluster.getClusterMembers().stream()
@@ -124,6 +129,7 @@ public class ControllerService {
                         .collect(Collectors.toList());
             } catch (ClusterException e) {
                 // cluster implementation throws checked exceptions which cannot be thrown inside completable futures.
+                log.error("Throwing Cluster Exception amit ::{}", e);
                 throw Exceptions.sneakyThrow(e);
             }
         }, executor);
